97080eff84f827f9368d0778644e6568cfe85da8
HiroYoungKun <97713039+HiroYoungKun@users.noreply.github.com>
2022-01-18 11:11:54 +0900

    Update Fromee_v4.py

--- Fromee_v4.py
+++ Fromee_v4.py
@@ -471,10 +471,10 @@ class micro_servis():
 st.write('# 会議からあなたの毎日を変える　Fromee')
 
 # if os.path.isfile("https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv") == True:
-    with open('https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv', 'w', newline="") as f:
-        printf("fuck")
-        writer = csv.writer(f)
-        writer.writerow(['満足度', '会議の種類', '予定時間'])
+#     with open('https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv', 'w', newline="") as f:
+#         printf("fuck")
+#         writer = csv.writer(f)
+#         writer.writerow(['満足度', '会議の種類', '予定時間'])
 
 df1 = pd.read_csv("https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv")
 # with open('https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv') as f:
