dbeb40630fb25d6394d0d0912cde1a7357023100
HiroYoungKun <97713039+HiroYoungKun@users.noreply.github.com>
2022-01-18 11:23:55 +0900

    Update Fromee_v4.py

--- Fromee_v4.py
+++ Fromee_v4.py
@@ -477,7 +477,7 @@ st.write('# 会議からあなたの毎日を変える　Fromee')
 #         writer.writerow(['満足度', '会議の種類', '予定時間'])
 
 # df1 = pd.read_csv("https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv")
-df1 = pd.read_csv("https://share.streamlit.io/hiroyoungkun/streamlitapps/main/test.csv")
+df1 = pd.read_csv("https://share.streamlit.io/hiroyoungkun/streamlitapps/main/Sample-Spreadsheet-10-rows.csv")
 print(df1)
 st.write(df1)
 satis = st.radio(
